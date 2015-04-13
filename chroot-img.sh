e9afc7e43eb6ebe3cd96074f06f2439ff35b9417
Sławomir Paszko <folkus@gmail.com>
2015-04-13 21:28:14 +0200

    Skrypt do wchodzenia tylko w chroot.

--- chroot-img.sh
+++ chroot-img.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
+# Montowanie i wejście do skryptu
 # Należy uruchamiać z sudo lub uprawnieniami root'a.
-# W przeciwniestwie do chroot-img wchodzi do zamontowanego systemu plików przy 
-# okazji montujacy proc,dev,sys.
 #
 # Argumenty :
 # $1 - argument czy "m"ontować czy nie "u"
 # $2 - nazwa obrazu,
-MNTDIR=$2
+IMGFILE=$2
+IMGNAME=`echo $IMGFILE | grep "[a-zA-Z0-9\.\_\-]*$" - `
+MNTDIR=/mnt/$IMGNAME.dir
 
 if [ $1 = "m" ] ; then
-		echo "Montowanie /proc /dev/ /sys."
+		echo "Montowanie systemu."
+		mkdir $MNTDIR
+		mount $IMGFILE $MNTDIR
 		mount --bind /proc $MNTDIR/proc
 		mount --bind /sys $MNTDIR/sys
 		mount --bind /dev $MNTDIR/dev
@@ -17,10 +20,12 @@ if [ $1 = "m" ] ; then
 		echo "Wchodzę do wnetrzna systemu.."
 		chroot $MNTDIR
 elif [ $1 = "u" ] ; then
-		echo "Odmontowuje /proc /dev /sys."
+		echo "Odmontowuje system."
 		umount $MNTDIR/proc
 		umount $MNTDIR/sys
 		umount $MNTDIR/dev
+		umount $MNTDIR
+		rm -rf $MNTDIR
 fi
 
 
