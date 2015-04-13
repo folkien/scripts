f2dc57e17adf7a2cc48c727657c935266eda5d84
folkien <folkus@gmail.com>
2015-04-13 14:37:21 +0200

    Dodanie opcji ze dla kazdego obrazu jest tworzony wlasny katalog i usuwany. Dzieki temu mozemy miec zamontowanych kilka obrazow jednoczesnie.

--- chroot.sh
+++ chroot.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 # Montowanie i wejście do skryptu
-MNTDIR=/mnt/gentoo
-#IMGFILE=mmr-imx.ext4
+# Należy uruchamiać z sudo lub uprawnieniami root'a.
+#
+# Argumenty :
+# $1 - argument czy "m"ontować czy nie "u"
+# $2 - nazwa obrazu,
 IMGFILE=$2
+IMGNAME=`echo $IMGFILE | grep "[a-zA-Z0-9\.\_\-]*$" - `
+MNTDIR=/mnt/$IMGNAME.dir
 
 if [ $1 = "m" ] ; then
 		echo "Montowanie systemu."
+		mkdir $MNTDIR
 		mount $IMGFILE $MNTDIR
 		mount --bind /proc $MNTDIR/proc
 		mount --bind /sys $MNTDIR/sys
@@ -19,6 +25,7 @@ elif [ $1 = "u" ] ; then
 		umount $MNTDIR/sys
 		umount $MNTDIR/dev
 		umount $MNTDIR
+		rm -rf $MNTDIR
 fi
 
 
