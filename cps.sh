8d2038e18567a72daa9fa6cacb792aaa739fb9b2
folkien <folkus@gmail.com>
2015-04-15 12:58:19 +0200

    Dodano wersje z pyNotify do cps.sh tak aby po długim kopiowaniu powiadomił mnie że praca została zakończona.

--- cps.sh
+++ cps.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 #kopiowanie z progressem, wymaga zainstalowanego rsync'a
 rsync --progress -a $1 $2
+
+command_exists () {
+    type "$1" &> /dev/null ;
+}
+
+if command_exists pyNotify ; then
+	pyNotify "Copy : Zakończono kopiowanie $1 do $2."
+fi
