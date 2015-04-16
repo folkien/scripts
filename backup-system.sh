b510e6dfc23d0923fb3a6b10bfc7f073bdca9d81
folkien <folkus@gmail.com>
2015-04-16 13:18:56 +0200

    Poprawki dla programu robiacego backup.

--- backup-system.sh
+++ backup-system.sh
@@ -5,8 +5,25 @@ VERSION=`lsb_release -a | grep Rele | grep ".[0-9]\..*" -o`
 KERNEL=`uname -r`
 SYSTEM=$SESSION
 
+if [ "$#" -eq 0 ] ; then
+	echo "Prosze podać argument np all - cały backup, none - każdy folder osobno."
+	exit 0
+fi
+
 echo "Tworze archwium w /tmp ..."
-tar cJpvf /tmp/$SYSTEM-$VERSION-$KERNEL.tar.xz --totals --exclude=/dev/* --exclude=/proc/* --exclude=/sys/* --exclude=/tmp/* --exclude=/home/* /
+if [ $1 = "all" ] ; then
+	tar cJpvf /tmp/$SYSTEM-$VERSION-$KERNEL.tar.xz  --exclude=/dev/* --exclude=/proc/* --exclude=/sys/* --exclude=/tmp/* --exclude=/home/* /
+else
+	tars /usr   /tmp/usr$SYSTEM-$VERSION-$KERNEL.tar.xz  
+	tars /etc   /tmp/etc$SYSTEM-$VERSION-$KERNEL.tar.xz  
+	tars /lib   /tmp/lib$SYSTEM-$VERSION-$KERNEL.tar.xz  
+	tars /lib32   /tmp/lib32$SYSTEM-$VERSION-$KERNEL.tar.xz  
+	tars /lib64   /tmp/lib64$SYSTEM-$VERSION-$KERNEL.tar.xz  
+	tars /boot   /tmp/boot$SYSTEM-$VERSION-$KERNEL.tar.xz  
+	tars /usr   /tmp/usr$SYSTEM-$VERSION-$KERNEL.tar.xz  
+	tars /var  /tmp/var$SYSTEM-$VERSION-$KERNEL.tar.xz  
+	tars /opt  /tmp/opt$SYSTEM-$VERSION-$KERNEL.tar.xz  
+fi
 echo "Archiwum stworzone!"
 
 
