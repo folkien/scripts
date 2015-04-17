15a63cd46167b7848b648bcf1c282d602b80c7d6
Sławomir Paszko <folkus@gmail.com>
2015-04-17 22:55:45 +0200

    Poprawki instalatora.

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ ln -s $DIR/cps.sh cps
 ln -s $DIR/git-create-repository.sh git-create-repository
 ln -s $DIR/tars.sh tars
 ln -s $DIR/trim.py trim
+ln -s $DIR/after.py after
  
 cd $DIR
 cp -rfv colors.sh /etc/
