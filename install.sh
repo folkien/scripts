2f4c4238826fea7fe9be541442538925cb78cad0
Sławomir Paszko <folkus@gmail.com>
2015-09-12 15:02:55 +0200

    Skrypt startujacy yakuake oraz kilka zmian uprawnien do wykonywania.:

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ ln -s $DIR/trim.py trim
 ln -s $DIR/after.py after
 ln -s $DIR/volumeup.sh volumeup
 ln -s $DIR/volumedown.sh volumedown
+ln -s $DIR/ysess.py ysess
  
 cd $DIR
 cp -rfv colors.sh /etc/
---
+++ yakuakestarter.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+/usr/bin/yakuake &
+wait
+sleep 3
+ysess -i ~/yakuake.ini 
+echo "Yakuake started."
+
