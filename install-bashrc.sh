56d64a98b90eb677caecd5ab820d1c776e9bce9e
folkien <folkus@gmail.com>
2015-10-21 11:52:30 +0200

    Dodano plik przechowujacy uzytkownika ktory instalował. W ten sposob zawsze wiemy z jakich katalogow includowac aliasy.

--- install-bashrc.sh
+++ install-bashrc.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 DIR=$(pwd)
 echo "source $DIR/.bashrc.base" >> /home/$USER/.bashrc
+
+#Tworzymy pliki konfiguracyjny
+sudo touch /etc/bashrc.conf
+sudo chown $USER /etc/bashrc.conf
+echo "INSTALLUSER=$USER" > /etc/bashrc.conf
