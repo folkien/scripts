1e56827021525024986a600f98ea22e09243ec6a
folkien <folkus@gmail.com>
2015-10-29 21:07:19 +0100

    Niepotrzebny grep.

--- install-vim-bitbake.sh
+++ install-vim-bitbake.sh
@@ -3,5 +3,4 @@ source ~/.bashrc
 mkdir -p ~/.vim/bundle/bitbake/
 cd ~/.vim/bundle/bitbake/
 git clone git@github.com:kergoth/vim-bitbake.git ./
-grep "bitbake" ~/.vimrc.bundles.local
 ln -sf $scripts/.vimrc.bundles.local ~/.vimrc.bundles.local
