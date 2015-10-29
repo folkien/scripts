46d3476df16a28448566c5c2423014e2b40bf6c5
folkien <folkus@gmail.com>
2015-10-29 11:36:41 +0100

    Dodano inny schemat kolorów dla gvim, inny dla vim.

--- install-vim.sh
+++ install-vim.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 source ~/.bashrc
 ln -sf $scripts/.vimrc.local ~/.vimrc.local
+ln -sf $scripts/.gvimrc.local ~/.gvimrc.local
 ln -sf $scripts/.vimrc.bundles.local ~/.vimrc.bundles.local
