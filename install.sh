9583c881b09e7eeb774be6337a930221716123e6
folkien <folkus@gmail.com>
2015-11-11 21:28:13 +0100

    Poprawki instalatora dla resize obrazków.

--- install.sh
+++ install.sh
@@ -18,4 +18,6 @@ ln -sf $DIR/chroot.sh mount-chroot
 ln -sf $DIR/append.sh append
 ln -sf $DIR/filesize.sh filesize
 ln -sf $DIR/focus_or_launch.sh focus_or_launch
+ln -sf $DIR/zdjecia-na-strone.sh zdjecia-na-strone
+ln -sf $DIR/image-resize.sh image-resize
 
