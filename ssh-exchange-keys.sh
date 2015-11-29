9ac2692965cc24b5cdbc2b1513b190b186f024d8
folkien <folkus@gmail.com>
2015-11-29 12:53:19 +0100

    Skrypt do szybkiego wymieniania kluczy pomiędzy hostami.

---
+++ ssh-exchange-keys.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Podajemy argument jak dla ssh np user@192.168.0.1
+
+if [ $# -lt 1 ]; then
+    echo "Za mało argumentów."
+    echo "Podajemy argument jak dla ssh np user@192.168.0.1"
+    exit 1
+fi
+
+DESTINATION=$@
+
+if [ -d /home/$USER/.ssh/ ]; then
+    echo "Wymieniam swój klucz."
+    LOCALKEY=$(cat /home/$USER/.ssh/id_rsa.pub)
+    ssh $DESITNATION 'echo $LOCALKEY >> ~/.ssh/authorized_keys'
+    echo "Pobieram zdalny klucz."
+    REMOTEKEY=$(ssh $DESITNATION 'cat ~/.ssh/id_rsa.pub')
+    echo $REMOTEKEY >> /home/$USER/.ssh/authorized_keys 
+else
+    echo "Local .ssh folder doesn't exists."
+fi
