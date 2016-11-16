e5deab7348a5cf9bb006b624d0b0aff96d117878
Sławomir Paszko <folkus@gmail.com>
2016-11-16 22:33:01 +0100

    Old way to make .configs.

--- .config/spasz-P55-UD3/variables.sh
+++
@@ -1,7 +0,0 @@
-echo "Variables sourced."
-
-#define the height in px of the top system-bar:
-export TOPMARGIN=30
-
-#sum in px of all horizontal borders:
-export RIGHTMARGIN=48
--- .config/spaszpc/variables.sh
+++
@@ -1,5 +0,0 @@
-#define the height in px of the top system-bar:
-export TOPMARGIN=30
-
-#sum in px of all horizontal borders:
-export RIGHTMARGIN=48
