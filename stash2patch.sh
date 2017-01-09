d38c87eb40e950d77ca266168e6f7f280d487b2f
Sławomir Paszko <folkus@gmail.com>
2017-01-09 16:28:30 +0100

    Changes in stash.

--- stash2patch.sh
+++ stash2patch.sh
@@ -1,5 +1,10 @@
 VERSION=$(date +%y%Vv%u)
-git stash
+#Checking if work is not stashed
+git status | grep  "Changes not staged for commit"
+if [ $? -eq 0 ]; then
+    echo "Stashing local work!"
+    git stash
+fi
 git stash show -p > ${VERSION}.patch
 if [ -z ${STASHDIRECTORY+x} ]; then
     echo "Stash directory is unset."
