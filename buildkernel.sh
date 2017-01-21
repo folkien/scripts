07d68c29b1f430ae371a382df505666847a02b56
Sławomir Paszko <folkus@gmail.com>
2017-01-21 22:19:07 +0100

    buildkernel saving settings and display kernel image filesize.

--- buildkernel.sh
+++ buildkernel.sh
@@ -1,5 +1,23 @@
-arm-make LOADADDR=0x18000000 zImage -j9 && \
+if [ -f .loadaddr  ]; then
+    M_LOADADDR=$(cat .loadaddr)
+else
+    echo "Give me loadaddr, e.g. 0x18000000 :"
+    read M_LOADADDR
+    echo $M_LOADADDR > .loadaddr
+fi
+
+if [ -f .imagetype ]; then
+    IMAGETYPE=$(cat .imagetype)
+else
+    echo "Give me imagetype e.g. zImage, uImage :"
+    read M_IMAGETYPE
+    echo $M_IMAGETYPE > .imagetype
+fi
+
+arm-make LOADADDR=${M_LOADADDR} ${M_IMAGETYPE} -j9 && \
 arm-make -j9 dtbs && \
 arm-make -j9 modules && \
 arm-make -j9 modules_install && \
 arm-make -j9 tarbz2-pkg
+echo "Image size:"
+ls -lah ./arch/arm/boot/${M_IMAGETYPE}
