34c246416593358ac204ef73902e3ea0efa72ad7
Sławomir Paszko <folkus@gmail.com>
2017-01-21 12:48:11 +0100

    Added switch for no dependencies install.'

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #instalacja skryptów w systemie
 DIR=`pwd`
+
+if [ $@ = "scripts" ]; then
+    NODEPENDENCIES=1
+fi
+
+echo "Scripts installation..."
 cp ./colors.sh /etc/
 cd /usr/bin/
 ln -sf $DIR/pushf.sh pushf
@@ -28,50 +34,54 @@ ln -sf $DIR/udevquery.sh udevquery
 ln -sf $DIR/grepsed.sh grepsed
 ln -sf $DIR/stash2patch.sh stash2patch
 ln -sf $DIR/ssh-exchange-keys.sh ssh-exchange-keys
+ln -sf $DIR/buildkernel.sh buildkernel
 
-#Package manager determining
-lsb_release -i | grep Ubuntu
-if [ $? -eq 0 ]; then
-    PKG_MANAGER="apt-get install -yf "
+if [ $NODEPENDENCIES -ne 1 ]; then
+    echo "Dependencies installation..."
+    #Package manager determining
+    lsb_release -i | grep Ubuntu
+    if [ $? -eq 0 ]; then
+        PKG_MANAGER="apt-get install -yf "
+    fi
+    lsb_release -i | grep Manjaro
+    if [ $? -eq 0 ]; then
+        PKG_MANAGER="pacman -S "
+    fi
+    lsb_release -i | grep Arch
+    if [ $? -eq 0 ]; then
+        PKG_MANAGER="pacman -S "
+    fi
+    # Dependencies install
+    $PKG_MANAGER pv dialog ncdu pydf
+    $PKG_MANAGER device-tree-compiler
+    $PKG_MANAGER yakuake
+    $PKG_MANAGER convert
+    $PKG_MANAGER git gitg qgit git-svn 
+    $PKG_MANAGER make
+    $PKG_MANAGER ncurses libncurses-dev
+    $PKG_MANAGER gcc-arm-linux-gnueabihf gcc-arm-linux-gnueabi
+    $PKG_MANAGER u-boot-tools
+
+    echo "Generating new ssh key. Please install it in github.com"
+    ssh-keygen
+    echo "Please install git certificate .ssh or add your own to github. Waiting."
+    read
+
+    cd $DIR
+    sh ./install-vim.sh
+
+    #Installation of mc
+    echo "MC configuration files."
+    cp -rfv ~/.config/mc ~/.config/mc.old
+    ln -sf $DIR/.config/mc/hotlist ~/.config/mc/hotlist
+    ln -sf $DIR/.config/mc/hotlist.bak ~/.config/mc/hotlist.bak
+    ln -sf $DIR/.config/mc/mc.ext ~/.config/mc/mc.ext
+    ln -sf $DIR/.config/mc/mc.keymap ~/.config/mc/mc.keymap
+    ln -sf $DIR/.config/mc/panels.ini ~/.config/mc/panels.ini
+    ln -sf $DIR/.config/mc/ini ~/.config/mc/ini
+
+    # Tworzenie najważniejszych katalogów
+    source $DIR/.bashrc.variables
+    mkdir -p $git $www $gdrive $boisko $software
+    chown -R $USER.$USER $software
 fi
-lsb_release -i | grep Manjaro
-if [ $? -eq 0 ]; then
-    PKG_MANAGER="pacman -S "
-fi
-lsb_release -i | grep Arch
-if [ $? -eq 0 ]; then
-    PKG_MANAGER="pacman -S "
-fi
-# Dependencies install
-$PKG_MANAGER pv dialog ncdu pydf
-$PKG_MANAGER device-tree-compiler
-$PKG_MANAGER yakuake
-$PKG_MANAGER convert
-$PKG_MANAGER git gitg qgit git-svn 
-$PKG_MANAGER make
-$PKG_MANAGER ncurses libncurses-dev
-$PKG_MANAGER gcc-arm-linux-gnueabihf gcc-arm-linux-gnueabi
-$PKG_MANAGER u-boot-tools
-
-echo "Generating new ssh key. Please install it in github.com"
-ssh-keygen
-echo "Please install git certificate .ssh or add your own to github. Waiting."
-read
-
-cd $DIR
-sh ./install-vim.sh
-
-#Installation of mc
-echo "MC configuration files."
-cp -rfv ~/.config/mc ~/.config/mc.old
-ln -sf $DIR/.config/mc/hotlist ~/.config/mc/hotlist
-ln -sf $DIR/.config/mc/hotlist.bak ~/.config/mc/hotlist.bak
-ln -sf $DIR/.config/mc/mc.ext ~/.config/mc/mc.ext
-ln -sf $DIR/.config/mc/mc.keymap ~/.config/mc/mc.keymap
-ln -sf $DIR/.config/mc/panels.ini ~/.config/mc/panels.ini
-ln -sf $DIR/.config/mc/ini ~/.config/mc/ini
-
-# Tworzenie najważniejszych katalogów
-source $DIR/.bashrc.variables
-mkdir -p $git $www $gdrive $boisko $software
-chown -R $USER.$USER $software
