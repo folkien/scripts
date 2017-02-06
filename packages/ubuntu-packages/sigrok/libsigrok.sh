c9d17e843e33432ac31f7c26584c74355088615e
Sławomir Paszko <folkus@gmail.com>
2017-02-06 12:02:19 +0100

    Fixed dependencies of libsigrok.

--- packages/ubuntu-packages/sigrok/libsigrok.sh
+++ packages/ubuntu-packages/sigrok/libsigrok.sh
@@ -1,4 +1,4 @@
-sudo apt-get install -yf git-core gcc g++ make autoconf autoconf-archive \
+sudo apt-get install -yf git-core gcc g++ make autoconf autoconf-archive libglibmm-2.4 \
   automake libtool pkg-config libglib2.0-dev libglibmm-2.4-dev libzip-dev libzip4-dev \
   libusb-1.0-0-dev libftdi-dev check doxygen python-numpy\
   python-dev python-gi-dev python-setuptools swig default-jdk
@@ -7,7 +7,7 @@ mkdir -p /opt/sigrok/
 if [ $? -eq 0 ]; then
     cd libsigrok
     ./autogen.sh
-    LD_LIBRARY_PATH=/opt/sigrok/lib/ PKG_CONFIG_PATH=/opt/sigrok/lib/pkgconfig/   ./configure --prefix=/opt/sigrok/
-    make
+    LD_LIBRARY_PATH=/opt/sigrok/lib/ PKG_CONFIG_PATH=/opt/sigrok/lib/pkgconfig/   ./configure --prefix=/opt/sigrok/ --enable-cxx
+    make -j4
     sudo PYTHONPATH=/opt/sigrok/lib/python2.7/site-packages/ make install
 fi
