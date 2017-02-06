511ddb4090d6a9ced8c08db82d029475b414fb5c
Sławomir Paszko <folkus@gmail.com>
2017-02-06 11:44:03 +0100

    Fixed libzip4-dev dependencies.

--- packages/ubuntu-packages/sigrok/libsigrok.sh
+++ packages/ubuntu-packages/sigrok/libsigrok.sh
@@ -1,5 +1,5 @@
 sudo apt-get install -yf git-core gcc g++ make autoconf autoconf-archive \
-  automake libtool pkg-config libglib2.0-dev libglibmm-2.4-dev libzip-dev \
+  automake libtool pkg-config libglib2.0-dev libglibmm-2.4-dev libzip-dev libzip4-dev \
   libusb-1.0-0-dev libftdi-dev check doxygen python-numpy\
   python-dev python-gi-dev python-setuptools swig default-jdk
 git clone git://sigrok.org/libsigrok
