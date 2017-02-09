6c06052be5033a0780fd8f883a4b9c94cf553d46
Sławomir Paszko <folkus@gmail.com>
2017-02-09 14:56:17 +0100

    Corrected libsigrok building because some drivers were missed because of libusb and libftdi.

--- packages/ubuntu-packages/sigrok/libsigrok.sh
+++ packages/ubuntu-packages/sigrok/libsigrok.sh
@@ -1,13 +1,14 @@
 sudo apt-get install -yf git-core gcc g++ make autoconf autoconf-archive libglibmm-2.4 \
   automake libtool pkg-config libglib2.0-dev libglibmm-2.4-dev libzip-dev libzip4-dev \
-  libusb-1.0-0-dev libftdi-dev check doxygen python-numpy\
-  python-dev python-gi-dev python-setuptools swig default-jdk
+  libusb-1.0-0-dev libftdi-dev check doxygen python-numpy \
+  python-dev python-gi-dev python-setuptools swig default-jdk libusb-1.0-0-dev \
+  libftdi-dev
 git clone git://sigrok.org/libsigrok
 mkdir -p /opt/sigrok/
 if [ $? -eq 0 ]; then
     cd libsigrok
     ./autogen.sh
-    LD_LIBRARY_PATH=/opt/sigrok/lib/ PKG_CONFIG_PATH=/opt/sigrok/lib/pkgconfig/   ./configure --prefix=/opt/sigrok/ --enable-cxx
-    make -j4
+    LD_LIBRARY_PATH=/opt/sigrok/lib/ PKG_CONFIG_PATH=/opt/sigrok/lib/pkgconfig/   ./configure --prefix=/opt/sigrok/ --enable-cxx --enable-all-drivers 
+    make -j5
     sudo PYTHONPATH=/opt/sigrok/lib/python2.7/site-packages/ make install
 fi
