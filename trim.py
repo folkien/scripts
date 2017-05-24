71083671e11a33077bb8916868a4b4ba6ff1809f
Sławomir Paszko <folkus@gmail.com>
2017-05-24 08:40:06 +0200

    Corrected trim.py to remove also internal spaces.

--- trim.py
+++ trim.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 #
-import sys, argparse, os
-
+import sys
 
 tekst = sys.stdin.read() #Czytamy wszystko z bufora
-tekst = tekst.strip()
+#tekst = tekst.strip() # remove spaces from start and end of string
+tekst = ' '.join(tekst.split()) # remove internal whitespaces
 sys.stdout.write(tekst)
 
