df4684b66e4078facfa481045164acb9ed376325
Sławomir Paszko <folkus@gmail.com>
2017-05-24 08:50:29 +0200

    Corrected trim to leave \n characters when trimming.

--- trim.py
+++ trim.py
@@ -4,6 +4,8 @@ import sys
 
 tekst = sys.stdin.read() #Czytamy wszystko z bufora
 #tekst = tekst.strip() # remove spaces from start and end of string
-tekst = ' '.join(tekst.split()) # remove internal whitespaces
+tekst = '\n'.join( [' '.join(s.split()) for s in tekst.splitlines()])
+#tekst = ' '.join(tekst.split(" ")) # remove internal whitespaces
+tekst += "\n"
 sys.stdout.write(tekst)
 
