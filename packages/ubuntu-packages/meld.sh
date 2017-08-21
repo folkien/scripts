f070984ce3b5825d0f0f133a86a27cb4347db3f9
Sławomir Paszko <s.paszko@softint.pl>
2017-08-21 16:25:12 +0200

    Added meld encoding.

---
+++ packages/ubuntu-packages/meld.sh
@@ -0,0 +1,2 @@
+# add more encodings
+gsettings set org.gnome.meld detect-encodings "['UTF-8', 'WINDOWS-1252', 'ISO-8859-15', 'UTF-16', 'WINDOWS-1253', 'KOI8-R']"
---
+++ packages/ubuntu-packages/svnwcrev.sh
@@ -0,0 +1,6 @@
+# TODO Not ended, https://unix.stackexchange.com/questions/59824/how-to-build-svnwcrev
+wget http://svnwcrev.tigris.org/files/documents/3444/44067/svnwcrev-1.0.tar.gz
+tar -xzvf svnwcrev-1.0.tar.gz
+sudo apt-get update
+sudo apt-get install libapr1-dev libsvn-dev
+cd svnwcrev-1.0
