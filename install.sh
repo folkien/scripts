a1df165c43c8ddc695b6dc9d6c96864d73f29e26
Sławomir Paszko <s.paszko@softint.pl>
2017-10-05 12:27:14 +0200

    Added cmd.exe simulator.

--- install.sh
+++ install.sh
@@ -53,6 +53,7 @@ ln -sf $DIR/ubootCopy2Disk.sh ubootCopy2Disk
 ln -sf $DIR/PopeHomilia.sh PopeHomilia
 ln -sf $DIR/map2html.py map2html
 ln -sf $DIR/mp4tomp3.sh mp4tomp3
+ln -sf $DIR/cmd.sh cmd
 
 if [ $NODEPENDENCIES -ne 1 ]; then
     echo "Dependencies installation..."
