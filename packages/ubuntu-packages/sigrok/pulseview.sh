7dff9c11d16f278a55da31b81d8cd8160f28164d
Sławomir Paszko <s.paszko@softint.pl>
2017-11-24 15:29:24 +0100

    Updated sigrok to newer vesrion.

--- packages/ubuntu-packages/sigrok/pulseview.sh
+++ packages/ubuntu-packages/sigrok/pulseview.sh
@@ -1,12 +1,12 @@
 sudo apt-get install -yf git-core g++ make cmake libtool pkg-config \
   libglib2.0-dev libqt4-dev libboost-test-dev libboost-thread-dev\
-  libboost-filesystem-dev libboost-system-dev libqt5svg5-dev
+  libboost-filesystem-dev libboost-system-dev libqt5svg5-dev libqt5xmlpatterns5-dev qtbase5-dev
 git clone git://sigrok.org/pulseview
 mkdir -p /opt/sigrok/
 if [ $? -eq 0 ]; then
     cd pulseview
-    git checkout pulseview-0.3.0
+    git checkout pulseview-0.4.0
     LD_LIBRARY_PATH=/opt/sigrok/lib/ PKG_CONFIG_PATH=/opt/sigrok/lib/pkgconfig/ cmake . -DCMAKE_INSTALL_PREFIX:PATH=/opt/sigrok/
-    make -j9
+    make -j5
     sudo make install
 fi
