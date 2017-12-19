611cbd60dd9d887c4bef8fcfe497c240cc6c3106
Sławomir Paszko <folkus@gmail.com>
2017-12-19 17:23:18 +0100

    Corrected arm make.

--- arm-make.sh
+++ arm-make.sh
@@ -5,5 +5,7 @@ make INSTALL_MOD_PATH=/tmp/ \
      ARCH=arm \
      LOADADDR=0x18000000 \
      CROSS_COMPILE=/usr/bin/arm-linux-gnueabihf- \
+     CC=/usr/bin/arm-linux-gnueabihf-gcc \
+     CXX=/usr/bin/arm-linux-gnueabihf-g++ \
      $@
 
