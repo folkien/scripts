fed8c7a3aec8a74578f14f3f569439f649bed125
Sławomir Paszko <s.paszko@softint.pl>
2017-12-19 18:14:51 +0100

    Corrected regular expression in prependTextBeforeClassMethod.sh .

--- prependTextBeforeClassMethod.sh
+++ prependTextBeforeClassMethod.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 TEXT=$1
 
-for f in ./*
+for f in ./*.cpp
 do
+    echo "Parsing $f."
     # class method search and replace
-    sed -i "s/\(.*\:.*)\)/${TEXT} \1/g" $f
+    sed -i "s/\(.*\:\:.*(.*)\s$\)/${TEXT} \1/g" $f
 done
 
