f3a8988c0495c300ebffad32c735933f8825845b
Sławomir Paszko <slawomir.paszko@elzab.pl>
2018-03-01 12:27:22 +0100

    Corrected sigrok dependencies.

--- packages/ubuntu-packages/sigrok/libsigrok.sh
+++ packages/ubuntu-packages/sigrok/libsigrok.sh
@@ -1,6 +1,6 @@
 sudo apt-get install -yf git-core gcc g++ make autoconf autoconf-archive libglibmm-2.4 \
   automake libtool pkg-config libglib2.0-dev libglibmm-2.4-dev libzip-dev libzip4-dev \
-  libusb-1.0-0-dev libftdi-dev check doxygen python-numpy \
+  libusb-1.0-0-dev libftdi-dev check doxygen python-numpy  libzip-dev \
   python-dev python-gi-dev python-setuptools swig default-jdk libusb-1.0-0-dev \
   libftdi-dev
 git clone git://sigrok.org/libsigrok
