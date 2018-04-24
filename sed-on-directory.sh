7ff649060f811318750cc7ce981502d76ef3d22d
Sławomir Paszko <s.paszko@softint.pl>
2018-04-24 10:26:45 +0200

    Renamed and fix of sed-on-directory.

--- sed-on-directory.sh
+++ sed-on-directory.sh
@@ -1,6 +1,9 @@
+#
+# assertion to not modify git repository hidden files
 ls .git &> /dev/null
 if [ $? == 0 ]; then
     echo "Here is .git repository! You cannot use this command in this folder!"
     exit -1
 fi
+# loop through all files inside directory and call sed with given parameters
 find ./ -type f -print0 | xargs -0 sed -i $@
