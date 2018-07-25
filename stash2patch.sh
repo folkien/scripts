902086ecedb410d15fa6ea065920d98006966f2f
Sławomir Paszko <s.paszko@softint.pl>
2018-07-25 22:30:27 +0200

    Added generation of unpushed last commits inside patch file. Added Question to ask before leave.

--- stash2patch.sh
+++ stash2patch.sh
@@ -2,6 +2,10 @@ VERSION=$(date +%y%Vv%u)
 RepoName=$(basename `git rev-parse --show-toplevel`)
 Filename=${RepoName}${VERSION}.patch
 
+# Clean all patches in current directory.
+mkdir -p .old.patches
+[ -e *.patch ] && mv ./*.patch .old.patches/
+
 #Checking if work is not stashed
 # check of not commited 
 git status | grep  "Changes not staged for commit"
@@ -16,10 +20,28 @@ if [ $? -eq 0 ]; then
     git stash
 fi
 
-git stash show -p > ${Filename}
+#format-patches from last git-svn commit to this moment
+echo "## Creating patch's from uncommited changes. ##"
+git format-patch git-svn
+cat *.patch > ${Filename}
+rm *-*.patch
+
+# Append patch from current stash
+echo "## Appending current stash to patch file. ##"
+git stash show -p >> ${Filename}
 if [ -z ${STASHDIRECTORY+x} ]; then
     echo "Stash directory is unset."
 else
     cp -fv ${Filename} ${STASHDIRECTORY}/
 fi
+
+# End of work
+echo "## Everything Done! ##"
+echo ""
+
+# question to ask before leave work
+echo "Questions:"
+read -e -p "Is PowerSupply taken? [y/n]"
+read -e -p "Is device taken? [y/n]"
+read -e -p "Is JTAG taken? [y/n]"
 
