48ff36a0e642ac93bbb80bead9a1fe4ee9e99fc2
Sławomir Paszko <s.paszko@softint.pl>
2018-07-31 08:13:08 +0200

    Added commentary to patch2stash.sh.

--- patch2stash.sh
+++ patch2stash.sh
@@ -1,9 +1,22 @@
 source /etc/messages.sh
+# get current repository name
 RepoName=$(basename `git rev-parse --show-toplevel`)
-
+# find last modified file with repository name
 FILE=$(ls $STASHDIRECTORY | grep ${RepoName} | sort -r | head -n 1)
+
+# Copy file to repository directory
 minfo "Last patch file is ${FILE}. Applying."
 cp ${STASHDIRECTORY}/${FILE} ./
+
+# TODO 
+# - check if tar.gz, then unpack
+# - Apply commits first,
+# - Apply stash secondly,
+CommitsFile=""
+StashFile=""
+
+
+# Apply stash patch
 git apply ./${FILE}
 if [ $? -ne 0 ]; then
     merror "Patch failed."
