5b1aebb9ae64e275e75521dd70fbebff02bcee46
Sławomir Paszko <slawomir.paszko@elzab.pl>
2018-08-23 11:30:30 +0200

    Added also rebase of SHA and stash checking.

--- git-edit.sh
+++ git-edit.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 # Git edycja N commitow na dotyłu gdzie N to pierwszy argument.
 NumberOfCommits=$1
+localChanges=0
 
-git stash
-git rebase -i HEAD~${NumberOfCommits}
+git status | grep "Changes not staged" > /dev/null
 if [ $? -eq 0 ]; then
-    git stash apply
+    localChanges=1
+    git stash
+fi
+
+re='^[0-9]+$'
+if [[ $NumberOfCommitsr =~ $re ]] ; then
+    git rebase -i HEAD~${NumberOfCommits}
+else
+    git rebase -i ${NumberOfCommits}
+fi
+
+if [ $? -eq 0 ]; then
+    if [ $localChanges -eq 1 ]; then
+        git stash apply
+    fi
 fi
