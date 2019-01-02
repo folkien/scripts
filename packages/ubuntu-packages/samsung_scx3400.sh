3fcb945b078164d5e263a6cd850d0fe56705e2bc
Sławomir Paszko <folkus@gmail.com>
2019-01-02 08:47:42 +0100

    Fixed lpadmin user adding.

--- packages/ubuntu-packages/samsung_scx3400.sh
+++ packages/ubuntu-packages/samsung_scx3400.sh
@@ -2,3 +2,6 @@ axel http://downloadcenter.samsung.com/content/DR/201407/20140709152519621/ULD_V
 tar -xvf ULD_V1.00.27.04.tar.gz
 cd uld
 sudo sh install.sh
+sudo usermod -a -G lp $USER
+sudo usermod -a -G lpadmin $USER
+
