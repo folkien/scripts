7831940ec5ceaf9b61a27e5f28ea8ceb426539c3
Sławomir Paszko <folkus@gmail.com>
2019-04-22 00:37:04 +0200

    Fixed dvd package to use snap vlc instead of base vlc.

--- packages/ubuntu-packages/dvdcodedcs.sh
+++ packages/ubuntu-packages/dvdcodedcs.sh
@@ -1,11 +1,14 @@
-sudo apt purge -yf vlc vlc-plugin-access-extra browser-plugin-vlc
+sudo snap remove vlc
+sudo apt purge -yf vlc vlc-bin vlc-plugin-access-extra browser-plugin-vlc
 sudo apt purge -yf libdvdnav4 libdvdread4 libdvd-pkg libdvdcss2 gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly
 sudo apt purge -yf ubuntu-restricted-extras
 sudo apt install -yf libdvdnav4 libdvdread4 libdvd-pkg libdvdcss2 gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly
 sudo dpkg-reconfigure -u libdvd-pkg
 sudo apt install -yf ubuntu-restricted-extras regionset
-sudo apt install -yf vlc vlc-plugin-access-extra browser-plugin-vlc
 echo "!!! Use region 2 for Europe !!!"
 sudo regionset
+# Ubuntu version is not working, using snap instead.
+#sudo apt install -yf vlc vlc-plugin-access-extra browser-plugin-vlc
+sudo snap install vlc
 # Check command
-sudo apt install -yf libdvdnav4 libdvdread4 libdvd-pkg libdvdcss2 gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly vlc ubuntu-restricted-extras
+# sudo apt install -yf libdvdnav4 libdvdread4 libdvd-pkg libdvdcss2 gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly ubuntu-restricted-extras
