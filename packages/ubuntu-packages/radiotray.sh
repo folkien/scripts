aaf5e68897b630c4af6976926adff9a24748fb34
Sławomir Paszko <folkus@gmail.com>
2019-05-09 11:43:26 +0200

    RadioTray installation for every user added.

--- packages/ubuntu-packages/radiotray.sh
+++ packages/ubuntu-packages/radiotray.sh
@@ -1,3 +1,13 @@
 sudo apt-get install radiotray
-mkdir -p /home/${USER}/.local/share/radiotray/
-ln -sf ${scripts}/.config/radiotray/bookmarks.xml /home/${USER}/.local/share/radiotray/bookmarks.xml
+
+#Install for every user
+for D in /home/*; 
+do
+    user=$(basename ${D})
+    echo "Install for ${D}. User ${user}."
+    sudo mkdir -p $D/.local/share/radiotray/
+    sudo ln -svf ${scripts}/.config/radiotray/bookmarks.xml ${D}/.local/share/radiotray/bookmarks.xml
+    sudo chown -R ${user}.${user} $D/.local/share/radiotray/
+done
+
+
