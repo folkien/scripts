a4ed87ed957bd08b654996bd9320bc159aba6512
Sławomir Paszko <folkus@gmail.com>
2019-05-09 16:58:59 +0200

    Installation changed.

--- install-systemd.sh
+++ install-systemd.sh
@@ -2,3 +2,9 @@
 sudo cp -rfv ./systemctl/*.sh /usr/bin/
 sudo cp -rfv ./systemctl/*.service /etc/systemd/system/
 sudo cp -rfv ./systemctl/*.timer /etc/systemd/system/
+# Enable all timers
+for file in ./systemctl/*.timer; do
+    name=$(basename ${file})
+    sudo systemctl start $name
+    sudo systemctl status $name
+done
