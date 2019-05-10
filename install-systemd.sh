b12aac4a071c142e589ba2dc6385e4020f01bf5d
Sławomir Paszko <folkus@gmail.com>
2019-05-10 14:28:14 +0200

    Installation fix.

--- install-systemd.sh
+++ install-systemd.sh
@@ -5,9 +5,10 @@ sudo cp -rfv ./systemctl/*.service /etc/systemd/system/
 # Enable all timers
 for file in ./systemctl/*.timer; do
     name=$(basename ${file})
+    sudo cp -rfv ${file} /etc/systemd/system/
     # If service exists then only copy
     if [ -e /etc/systemd/system/${name} ]; then
-        sudo cp -rfv ${file} /etc/systemd/system/
+        echo "Exists. Updated ${name}!"
     #If not exists then start also
     else
         sudo systemctl enable $name
