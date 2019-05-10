6f9ba24d923b1aba4c24ca11e934780139c6997b
Sławomir Paszko <folkus@gmail.com>
2019-05-10 09:47:20 +0200

    Added timers enable/start for systemd.

--- install-systemd.sh
+++ install-systemd.sh
@@ -1,10 +1,18 @@
 # Script with installation of all systemd scripts.
 sudo cp -rfv ./systemctl/*.sh /usr/bin/
 sudo cp -rfv ./systemctl/*.service /etc/systemd/system/
-sudo cp -rfv ./systemctl/*.timer /etc/systemd/system/
+
 # Enable all timers
 for file in ./systemctl/*.timer; do
     name=$(basename ${file})
-    sudo systemctl start $name
-    sudo systemctl status $name
+    # If service exists then only copy
+    if [ -e /etc/systemd/system/${name} ]; then
+        sudo cp -rfv ${file} /etc/systemd/system/
+    #If not exists then start also
+    else
+        sudo systemctl enable $name
+        sudo systemctl start $name
+        sudo systemctl status $name
+    fi
 done
+
