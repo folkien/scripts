a291626a56709cbb6f2bae037537ffa0557e6c1b
Sławomir Paszko <folkus@gmail.com>
2019-05-11 13:07:23 +0200

    sysdig install only when not exists.

--- packages/ubuntu-packages/sysdig.sh
+++ packages/ubuntu-packages/sysdig.sh
@@ -1,6 +1,8 @@
 # Sysdig - program do analizy systemu. S.Paszko
 # Przed instalacja nalezy sprawdzic czy lista repozytoriow w systemie jest 
 # poprawna ( czy działa apt-get update i nie zwraca bledow).
-curl -s https://s3.amazonaws.com/download.draios.com/stable/install-sysdig | sudo bash
-
-sudo apt-get -yf install sysdig
+sysdig --version
+if [ $? -ne 0 ]; then
+    curl -s https://s3.amazonaws.com/download.draios.com/stable/install-sysdig | sudo bash
+    sudo apt-get -yf install sysdig
+fi
