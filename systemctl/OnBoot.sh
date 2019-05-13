bb6c038ac64ad8ecb9f4d068aac78668750bbb26
Sławomir Paszko <folkus@gmail.com>
2019-05-13 23:24:29 +0200

    Added auto-update of repos script.

--- systemctl/OnBoot.sh
+++ systemctl/OnBoot.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 # Script run every 3 hours
 notify-send-all "Update repozytoriów!"
+
+# Update scripts
+echo "Update of scripts."
+cd ${scripts}
+git pull --rebase
+
+# Update python repos
+for directory in /home/${USER}/python/*; do
+    if [ -d ${directory} ]; then
+        echo "Update of ${directory}."
+        cd ${directory}
+        git pull --rebase
+    fi
+done
