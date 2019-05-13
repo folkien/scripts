7156d7d17349dbeb4220f5bea974900c9a52a6a5
Sławomir Paszko <folkus@gmail.com>
2019-05-13 23:19:26 +0200

    Corrected notify-send-all script.

--- notify-send-all.sh
+++ notify-send-all.sh
@@ -5,7 +5,7 @@ notify-send-all(){
         dbus=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$p/environ | sed 's/DBUS_SESSION_BUS_ADDRESS=//')
         user=$(grep -m 1 -z USER /proc/$p/environ | sed 's/USER=//')
         dply=$(grep -z DISPLAY /proc/$p/environ | sed 's/DISPLAY=//')
-        sudo -u $user sh -c "DBUS_SESSION_BUS_ADDRESS=\"$dbus\" DISPLAY=\"$dply\" notify-send \"$text\""
+        sudo -u $user sh -c "DBUS_SESSION_BUS_ADDRESS=\"$dbus\" DISPLAY=\"$dply\" notify-send \"Powiadomienie\" \"$text\""
     done
 }
 
