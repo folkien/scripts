d28d916dde4105f32b59331907fa1c595bdce7cd
Sławomir Paszko <folkus@gmail.com>
2019-05-29 09:54:57 +0200

    Added missed.

--- install.sh
+++ install.sh
@@ -236,6 +236,7 @@ if [ $# -gt 0 ] && [ $Argument = "all" ]; then
     #Narzędzia sieciowe
     $PKG_MANAGER iperf
     $PKG_MANAGER net-tools
+    $PKG_MANAGER nmap
     $PKG_MANAGER wireshark
     $PKG_MANAGER filezilla
     $PKG_MANAGER rdesktop
