5b783084a50052a02fa94fffc2595d903adb7d5d
Sławomir Paszko <folkus@gmail.com>
2019-05-29 11:38:11 +0200

    Corrected samsung install script.

--- packages/ubuntu-packages/samsung_scx3400.sh
+++ packages/ubuntu-packages/samsung_scx3400.sh
@@ -1,7 +1,11 @@
-axel http://downloadcenter.samsung.com/content/DR/201407/20140709152519621/ULD_V1.00.27.04.tar.gz
-tar -xvf ULD_V1.00.27.04.tar.gz
-cd uld
-sudo sh install.sh
+#install if not exists or given arg
+if [ ! -e /opt/samsung ] || [ "$1" == "all" ]; then
+    axel http://downloadcenter.samsung.com/content/DR/201407/20140709152519621/ULD_V1.00.27.04.tar.gz
+    tar -xvf ULD_V1.00.27.04.tar.gz
+    cd uld
+    sudo sh install.sh
+fi
+echo "Changing permissions."
 sudo usermod -a -G lp $USER
 sudo usermod -a -G lpadmin $USER
 sudo chown root.root -R /opt/smfp-common/
