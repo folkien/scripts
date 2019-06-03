52484892b29dcda19d478cc1a23f06d335182e01
Sławomir Paszko <folkus@gmail.com>
2019-06-03 13:05:20 +0200

    Trace last file name @end.

--- trace-last.sh
+++ trace-last.sh
@@ -1,10 +1,11 @@
 #Find and trace last trace
+source /etc/messages.sh
 unset -v latest
 for dir in `find . -type d`
 do
     file=${dir}/trace.txt
     [ -e ${file} ] && [[ $file -nt $latest ]] && latest=$file
 done
-echo "Latest trace : $latest."
 cat ${latest}
+minfo "Latest trace : $latest."
 
