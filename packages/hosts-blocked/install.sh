ea443a6be11e32ff889f1db2de5c1431e7d5e888
Sławomir Paszko <folkus@gmail.com>
2019-07-09 11:33:35 +0200

    Corrected hosts installation.

--- packages/hosts-blocked/install.sh
+++ packages/hosts-blocked/install.sh
@@ -3,7 +3,10 @@ if [ -e used.txt ]; then
     while true; do
         read -p "Use used txt? y/n" yn
         case $yn in
-            [Yy]* ) sudo cp ./hosts.original /etc/hosts; sudo cat used.txt >> /etc/hosts; exit;;
+            [Yy]* ) 
+                    sudo cp ./hosts.original /etc/hosts; sudo cat used.txt >> /etc/hosts
+                    exit
+                ;;
             [Nn]* ) break;;
             * ) echo "Please answer yes or no.";;
         esac
@@ -20,10 +23,24 @@ do
     read -p "Block ${host}? y/n : " yn
     case $yn in
         [Yy]* )
-                echo "0.0.0.0 ${host}" >> used.txt
-                echo "0.0.0.0 www.${host}" >> used.txt
+                echo "0.0.0.0 ${host}.pl" >> used.txt
+                echo "0.0.0.0 www.${host}.pl" >> used.txt
+                echo "::0 ${host}.pl" >> used.txt
+                echo "::0 www.${host}.pl" >> used.txt
+                echo "0.0.0.0 ${host}.com" >> used.txt
+                echo "0.0.0.0 www.${host}.com" >> used.txt
+                echo "::0 ${host}.com" >> used.txt
+                echo "::0 www.${host}.com" >> used.txt
+                echo "0.0.0.0 ${host}.com.pl" >> used.txt
+                echo "0.0.0.0 www.${host}.com.pl" >> used.txt
+                echo "::0 ${host}.com.pl" >> used.txt
+                echo "::0 www.${host}.com.pl" >> used.txt
             ;;
         [Nn]* ) ;;
         * ) echo "Please answer yes or no.";;
     esac
 done
+
+# Update hosts
+cp -rfv hosts.original /etc/hosts
+cat used.txt >> /etc/hosts
