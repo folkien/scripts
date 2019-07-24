92f59d9cca7b2c1c18a29f0c762c1fbadb70a423
Sławomir Paszko <folkus@gmail.com>
2019-07-24 11:31:04 +0200

    HardFautl url fix, grep fix added log.

--- hardfault-find.sh
+++ hardfault-find.sh
@@ -9,22 +9,24 @@ url=$1
 hashToFind=$2
 [ $# -ge 3 ] && showGrepResult=1 || showGrepResult=0
 [ $# -ge 4 ] && grepSurrounding=$4 || grepSurrounding=0
+
 # Get index URL
 wget ${url} -O index.html &> /dev/null
-grep -o "href=\"\/hf\/.*\"" index.html | cut -c 7- | rev | cut -c 2- | rev | while read -r deviceUrl ;
+grep -a -o "href=\"\/hf\/.*\"" index.html | cut -c 7- | rev | cut -c 2- | rev | while read -r deviceUrl ;
 do
     # Get device website
     wget "${url}${deviceUrl}" -O tmpDevice.html &> /dev/null
-    hfAmount=$(grep "tr id.*" tmpDevice.html | wc -l)
-    emptyCommentAmount=$(grep "id=\"comment\"" tmpDevice.html | grep value=\"\" | wc -l)
+    hfAmount=$(grep -a "tr id.*" tmpDevice.html | wc -l)
+    emptyCommentAmount=$(grep -a "id=\"comment\"" tmpDevice.html | grep value=\"\" | wc -l)
     commentAmount=$(($hfAmount-$emptyCommentAmount))
-    grep -o "href=\"/hf\/.*\"" tmpDevice.html | cut -c 7- | rev | cut -c 2- | rev | while read -r hfUrl ;
+    grep -a -o "href=\"/hf\/.*\"" tmpDevice.html | cut -c 7- | rev | cut -c 2- | rev | while read -r hfUrl ;
     do
         # Get HF website
         wget "${url}${hfUrl}" -O tmpHf.html &> /dev/null
-        [ ${showGrepResult} -eq 1 ] && grep -C ${grepSurrounding} "${hashToFind}" tmpHf.html || grep "${hashToFind}" tmpHf.html &> /dev/null
+        [ ${showGrepResult} -eq 1 ] && grep -a -C ${grepSurrounding} "${hashToFind}" tmpHf.html || grep -a "${hashToFind}" tmpHf.html &> /dev/null
         if [ $? -eq 0 ]; then
-            echo "${url}${hfUrl} (${commentAmount}/${hfAmount}) find ${hashToFind}."
+            hfUrl=${hfUrl// /%20}
+            echo "# \"${url}${hfUrl}\" (${commentAmount}/${hfAmount}) find ${hashToFind}." | tee HF.log
         fi
     done
 done
