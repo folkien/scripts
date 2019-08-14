56a55711469c00a2d3fd74953b3de61868e2e176
Sławomir Paszko <folkus@gmail.com>
2019-08-14 14:20:42 +0200

    Added maxNextCounter reset.

--- Katechezy.sh
+++ Katechezy.sh
@@ -20,7 +20,8 @@ isNewDocument=0
 # Loop download of all katechesis
 number=1
 isDocumentInProgress=1
-tryNextCounter=3
+maxNextCounter=3
+tryNextCounter=${maxNextCounter}
 lastDocument=""
 while [ ${isDocumentInProgress} -eq 1 ]; do
     WEBSITE="https://www.deon.pl/religia/serwis-papieski/dokumenty/katechezy-audiencje/art,${number}.html"
@@ -28,12 +29,13 @@ while [ ${isDocumentInProgress} -eq 1 ]; do
     # If katechesis doesn't exists
     if [ ! -e ${outFilePath} ]; then
         echo "Download ${WEBSITE}."
-        wget ${WEBSITE} -O current.html > /dev/null
+        wget ${WEBSITE} -O current.html &> /dev/null
         if [ $? -eq 0 ]; then
             # Page exists, we can parse it
             xidel current.html -e "<div class='tresc'>{.}</div>*" > ${outFilePath}
             isNewDocument=1
             lastDocument=${outFilePath}
+            tryNextCounter=${maxNextCounter}
         else
             # 404 Error
             tryNextCounter=$((${tryNextCounter}-1))
@@ -46,7 +48,7 @@ done
 rm current.html
 
 # If there is new document then show it
-if [ ${isNewDocument} ]; then
+if [ ${isNewDocument} -eq 1 ]; then
     kate ${lastDocument} &
 fi
 exit 0
