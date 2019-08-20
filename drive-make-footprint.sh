294055c998b60f6b876df62d5a111d46422c6ce4
Sławomir Paszko <folkus@gmail.com>
2019-08-20 16:33:22 +0200

    Added 10MB footprint.

--- drive-make-footprint.sh
+++ drive-make-footprint.sh
@@ -5,5 +5,6 @@ if [ -e ${drive} ]; then
     sudo dd if=${drive} of=${prefix}_512B.bin bs=512 count=1
     sudo dd if=${drive} of=${prefix}_4KB.bin bs=4096 count=1
     sudo dd if=${drive} of=${prefix}_1MB.bin bs=1MB count=1
+    sudo dd if=${drive} of=${prefix}_10MB.bin bs=10MB count=1
 fi
 
