dcfc01e0c9dd9821ccac9c1592eba23238685bb8
Sławomir Paszko <folkus@gmail.com>
2019-08-20 16:37:38 +0200

    Added txt dumps and assert.

--- drive-make-footprint.sh
+++ drive-make-footprint.sh
@@ -1,10 +1,18 @@
+[ $# -eq 0 ] && echo "Missing prefix" && exit -1
+
 prefix="${1}"
 drive=$(usb-select-drive)
 
 if [ -e ${drive} ]; then
+    # binary dumps
     sudo dd if=${drive} of=${prefix}_512B.bin bs=512 count=1
     sudo dd if=${drive} of=${prefix}_4KB.bin bs=4096 count=1
     sudo dd if=${drive} of=${prefix}_1MB.bin bs=1MB count=1
     sudo dd if=${drive} of=${prefix}_10MB.bin bs=10MB count=1
+    # hexdumps
+    hexdump -C ${prefix}_512B.bin > ${prefix}_512B.bin.txt
+    hexdump -C ${prefix}_4KB.bin >  ${prefix}_4KB.bin.txt
+    hexdump -C ${prefix}_1MB.bin >  ${prefix}_1MB.bin.txt
+    hexdump -C ${prefix}_10MB.bin > ${prefix}_10MB.bin.txt
 fi
 
