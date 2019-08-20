46ab13dfc0a0c222dbb7bd6810e201f155222d2a
Sławomir Paszko <folkus@gmail.com>
2019-08-20 16:48:40 +0200

    Change to save partially 1MB's.

--- drive-make-footprint.sh
+++ drive-make-footprint.sh
@@ -7,12 +7,15 @@ if [ -e ${drive} ]; then
     # binary dumps
     sudo dd if=${drive} of=${prefix}_512B.bin bs=512 count=1
     sudo dd if=${drive} of=${prefix}_4KB.bin bs=4096 count=1
-    sudo dd if=${drive} of=${prefix}_1MB.bin bs=1MB count=1
-    sudo dd if=${drive} of=${prefix}_10MB.bin bs=10MB count=1
     # hexdumps
     hexdump -C ${prefix}_512B.bin > ${prefix}_512B.bin.txt
     hexdump -C ${prefix}_4KB.bin >  ${prefix}_4KB.bin.txt
-    hexdump -C ${prefix}_1MB.bin >  ${prefix}_1MB.bin.txt
-    hexdump -C ${prefix}_10MB.bin > ${prefix}_10MB.bin.txt
+    # First ten 1MB;s
+    for i in {0..9}
+    do
+        filePattern="${prefix}_${i}_1MB.bin"
+        sudo dd if=${drive} of=${filePattern} bs=1MB count=1 skip=${i}
+        hexdump -C ${filePattern} > ${filePattern}.txt
+    done
 fi
 
