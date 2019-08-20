e00b1df621822e7dc4a4992a41797afd37c3ec11
Sławomir Paszko <folkus@gmail.com>
2019-08-20 16:24:23 +0200

    Sudo added.

--- drive-make-footprint.sh
+++ drive-make-footprint.sh
@@ -2,8 +2,8 @@ prefix="${1}"
 drive=$(usb-select-drive)
 
 if [ -e ${drive} ]; then
-    dd if=${drive} of=${prefix}_512B.bin bs=512 count=1
-    dd if=${drive} of=${prefix}_4KB.bin bs=4096 count=1
-    dd if=${drive} of=${prefix}_1MB.bin bs=1MB count=1
+    sudo dd if=${drive} of=${prefix}_512B.bin bs=512 count=1
+    sudo dd if=${drive} of=${prefix}_4KB.bin bs=4096 count=1
+    sudo dd if=${drive} of=${prefix}_1MB.bin bs=1MB count=1
 fi
 
