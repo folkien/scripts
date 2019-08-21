3786b6f3ad5e6f75a9d70a40f2a7e6b4b80c02ef
Sławomir Paszko <folkus@gmail.com>
2019-08-21 10:36:26 +0200

    Added partition reader and dumper with partition founding.

--- drive-make-footprint.sh
+++ drive-make-footprint.sh
@@ -4,6 +4,20 @@ prefix="${1}"
 drive=$(usb-select-drive)
 
 if [ -e ${drive} ]; then
+    driveName=$(basename ${drive})
+    # partition info
+    sudo fdisk -l ${drive} > ${prefix}_partinfo.txt
+    sudo fdisk -l ${drive} | grep "${drive}[0-9]" | while read -r line; 
+    do
+        sectorSize=$(cat /sys/block/${driveName}/queue/hw_sector_size)
+        dumpSize=$((1024*1024/${sectorSize}))
+        partition=$(echo ${line} | grep "${drive}[0-9]" -o)
+        startPartition=$(echo ${line} | cut -c 10- | grep -o -E '[0-9]+' | head -1)
+        startPartByteAddress=$((${startPartition} * ${sectorSize}))
+        echo "Found partition ${partition} starts ${startPartition} (${startPartByteAddress})."
+        sudo dd if=${drive} of=${prefix}_partinfo.bin count=${dumpSize} skip=${startPartition}
+        hexdump -C ${prefix}_partinfo.bin > ${prefix}_partinfo.bin.txt
+    done
     # binary dumps
     sudo dd if=${drive} of=${prefix}_512B.bin bs=512 count=1
     sudo dd if=${drive} of=${prefix}_4KB.bin bs=4096 count=1
