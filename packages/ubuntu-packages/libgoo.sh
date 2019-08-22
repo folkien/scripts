16eca4d14b5391f4b84687557d730d934466f01f
Sławomir Paszko <folkus@gmail.com>
2019-08-22 22:30:08 +0200

    Libgoo added.

---
+++ packages/ubuntu-packages/libgoo.sh
@@ -0,0 +1,5 @@
+wget https://launchpad.net/ubuntu/+archive/primary/+files/libgoocanvas-common_1.0.0-1_all.deb
+wget https://launchpad.net/ubuntu/+archive/primary/+files/libgoocanvas3_1.0.0-1_amd64.deb
+wget https://launchpad.net/ubuntu/+archive/primary/+files/libgoo-canvas-perl_0.06-2ubuntu3_amd64.deb
+
+sudo dpkg -i libgoo*deb
