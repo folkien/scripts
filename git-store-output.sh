43ed6a3ea59a1d70fef6eb3c9397c68c46f0297c
Sławomir Paszko <folkus@gmail.com>
2019-08-22 13:51:20 +0200

    Git store branch added.

--- git-store-output.sh
+++ git-store-output.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 if [ $# -lt 1 ]; then
-    echo "Give directory!"
+    echo "Give output .elf directory!"
+    exit -1
 fi
+
 path=$1
-hash=$(git rev-parse HEAD)
-mkdir -p store/${hash}.${path}
-cp -rfv ${path}/*.map store/${hash}.${path}/
-cp -rfv ${path}/*.hex store/${hash}.${path}/
-cp -rfv ${path}/*.bin store/${hash}.${path}/
-cp -rfv ${path}/*.elf store/${hash}.${path}/
+branchFrom=$(git symbolic-ref -q HEAD)
+githash=$(git rev-parse HEAD)
+storeDir="store/${branchFrom}/${githash}.${path}"
+mkdir -p ${storeDir}
+cp -rfv ${path}/*.map ${storeDir}
+cp -rfv ${path}/*.hex ${storeDir}
+cp -rfv ${path}/*.bin ${storeDir}
+cp -rfv ${path}/*.elf ${storeDir}
 
