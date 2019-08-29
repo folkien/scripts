0be9e1e83eaa5c9c5b58da3300e08b603db35435
Sławomir Paszko <folkus@gmail.com>
2019-08-29 11:07:03 +0200

    Readme git script fix.

--- git-readme-update.sh
+++ git-readme-update.sh
@@ -1,11 +1,16 @@
-
-
 #Path directory check
 readmePath="README.md"
 pwd | grep Sources
 [ $? -eq 0 ] && readmePath="../${readmePath}"
 
+# Last commit message
+lastCommitMessage=$(git log --pretty=oneline | head -n 1)
+echo "*    commit ${lastCommitMessage}" >> ${readmePath}
+
+# open Readme to edit
 gvim -f ${readmePath}
+
+# Create readme update commit
 description=$(git diff ${readmePath} | grep "+.*commit" | grep "commit.*" -o)
 git add ${readmePath}
 git commit -m "README.md update about ${description}."
