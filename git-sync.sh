8ec9e7b81ddcc2890d32fcf3e2042d5771dfd260
Sławomir Paszko <folkus@gmail.com>
2019-09-06 16:22:54 +0200

    Fix.

--- git-sync.sh
+++ git-sync.sh
@@ -1,8 +1,10 @@
-[ ! -e .git  ] && exit 0
+git status
+[ $? -ne 0 ] && exit 0
 source /etc/messages.sh
 stashBefore=$(git stash | wc -l)
 git stash 
 stashAfter=$(git stash | wc -l)
-git pull --rebase && git push 
+git pull --rebase
+git push 
 [ ! ${stashBefore} -eq ${stashAfter} ] && git stash apply
 minfo 'Updated log :' && git log --pretty=oneline | head -n 5
