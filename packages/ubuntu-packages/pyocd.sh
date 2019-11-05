6929c8b6a183868410291d3afeb978828a1641bb
Sławomir Paszko <folkus@gmail.com>
2019-11-05 18:19:30 +0100

    Added extra pyocd rules.

--- packages/ubuntu-packages/pyocd.sh
+++ packages/ubuntu-packages/pyocd.sh
@@ -1,7 +1,4 @@
 sudo -H pip install pyocd
-sudo cat <<EOF > /etc/udev/rules.d/50-mbed.rules
-# 0d28:0204 NXP FRDM-K64F
-SUBSYSTEM=="usb", ATTR{idVendor}=="0d28", ATTR{idProduct}=="0204", MODE:="666"
-EOF
+sudo cp -rfv pyocd-udev/*.rules /etc/udev/rules.d
 sudo udevadm control --reload-rules && sudo udevadm trigger
 pyocd-tool list
