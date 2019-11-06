d0a5eacd777776378ec1de0cc6c0a3dbbfe92841
Sławomir Paszko <folkus@gmail.com>
2019-11-06 11:25:39 +0100

    Fix of version.

--- packages/ubuntu-packages/hg-export.sh
+++ packages/ubuntu-packages/hg-export.sh
@@ -1,6 +1,8 @@
 cd ${boisko}
 git clone https://github.com/frej/fast-export.git
 sudo ln -sf ${boisko}/fast-export/hg-fast-export.sh  /usr/bin/hg-fast-export
+cd fast-export
+git checkout v180317
 
 
 
