b0615d3442f15ae8fd8fc5adf9eac29d8f9c352e
Sławomir Paszko <folkus@gmail.com>
2019-11-08 13:32:01 +0100

    Fixed nmap call and ssh pseudo tty allocation + verbose.

--- ssh-find-login.sh
+++ ssh-find-login.sh
@@ -10,8 +10,8 @@ password="${2}"
 gatewayIP=$(route -n | grep UG | xargs -d " " -I {} echo {} | grep 0.0.0.0 -v | grep -E "([0-9]{1,3}[\.]){3}[0-9]{1,3}")
 ipArray=(${gatewayIP//./ })
 echo "Gateway IP - ${ipArray[0]}.${ipArray[1]}.${ipArray[2]}.${ipArray[3]}."
-nmap -sS -p 22 ${ipArray[0]}.${ipArray[1]}.${ipArray[2]}.1-255 | grep report | grep -o "${ipArray[0]}.${ipArray[1]}.${ipArray[2]}.*" | while read ipHost
+sudo -H nmap -sS -p 22 ${ipArray[0]}.${ipArray[1]}.${ipArray[2]}.1-255 | grep report | grep -o "${ipArray[0]}.${ipArray[1]}.${ipArray[2]}.*" | while read ipHost
 do
     echo "Try ${login}@${ipHost}."
-    sshpass -p "${password}" ssh -oStrictHostKeyChecking=no ${login}@${ipHost}
+    sshpass -p "${password}" ssh -tt -v -oStrictHostKeyChecking=no ${login}@${ipHost}
 done
