2c451d599b5ebf8342302220a520b70365386770
Sławomir Paszko <folkus@gmail.com>
2019-11-08 13:16:01 +0100

    SSH finder added.

---
+++ ssh-find.sh
@@ -0,0 +1,5 @@
+# Scan all ip's and find ping
+gatewayIP=$(route -n | grep UG | xargs -d " " -I {} echo {} | grep 0.0.0.0 -v | grep -E "([0-9]{1,3}[\.]){3}[0-9]{1,3}")
+ipArray=(${gatewayIP//./ })
+echo "Gateway IP - ${ipArray[0]}.${ipArray[1]}.${ipArray[2]}.${ipArray[3]}."
+nmap -sS -p 22 ${ipArray[0]}.${ipArray[1]}.${ipArray[2]}.1-255 | grep report
