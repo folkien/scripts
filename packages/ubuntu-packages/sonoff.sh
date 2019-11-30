3db6a1bec54066ea14dbce6b5ec3afd80bc97e1d
Sławomir Paszko <folkus@gmail.com>
2019-11-30 23:33:55 +0100

    Fixes.

--- packages/ubuntu-packages/sonoff.sh
+++ packages/ubuntu-packages/sonoff.sh
@@ -2,9 +2,9 @@ pip3 install PySide2
 pip3 install zeroconf
 cd ${git}
 if [ ! -e Sonoff_Devices_DIY_Tools ]; then
-    git clone git@github.com:itead/Sonoff_Devices_DIY_Tools.git
+    git clone git@github.com:folkien/Sonoff_Devices_DIY_Tools.git
     cd Sonoff_Devices_DIY_Tools
-    g co v3.3.0
+    g co v3.3.1
     cd  code/
     ./install.sh
 fi
