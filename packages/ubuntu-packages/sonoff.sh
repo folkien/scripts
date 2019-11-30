2de3569946fa90d453aef3482430030affd1a677
Sławomir Paszko <folkus@gmail.com>
2019-11-30 23:49:14 +0100

    Fix.

--- packages/ubuntu-packages/sonoff.sh
+++ packages/ubuntu-packages/sonoff.sh
@@ -4,7 +4,7 @@ cd ${git}
 if [ ! -e Sonoff_Devices_DIY_Tools ]; then
     git clone git@github.com:folkien/Sonoff_Devices_DIY_Tools.git
     cd Sonoff_Devices_DIY_Tools
-    g co v3.3.2
+    git co v3.3.2
     cd  code/
     ./install.sh
 fi
