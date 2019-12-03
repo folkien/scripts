1f8e3b3e5fd6f2c60d742ce9df6023029eda2aa1
Sławomir Paszko <folkus@gmail.com>
2019-12-03 10:11:49 +0100

    Fixed path and path checking added.

--- packages/ubuntu-packages/eclipse-oxygen.sh
+++ packages/ubuntu-packages/eclipse-oxygen.sh
@@ -5,4 +5,7 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
   [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
-ln -sfv ${DIR}/reclipse/eclipse-oxygen.ini /opt/eclipse/eclipse.ini
+
+if [ -e /opt/eclipse/ ]; then
+    ln -sfv ${DIR}/eclipse/eclipse-oxygen.ini /opt/eclipse/eclipse.ini
+fi
