413b38149c9d3a246bcf571fc23ab5ed96fe2ff3
Sławomir Paszko <folkus@gmail.com>
2019-12-04 11:26:55 +0100

    Small modifications.

--- packages/ubuntu-packages/espressif.sh
+++ packages/ubuntu-packages/espressif.sh
@@ -1,9 +1,12 @@
+VERSION="v4.0-beta2"
+
 #Pre requistes
 sudo apt-get install -yf git wget flex bison gperf python python-pip python-setuptools python-serial python-click python-cryptography python-future python-pyparsing python-pyelftools cmake ninja-build ccache
 mkdir -p /opt/espressif/
+
+# installation
 cd /opt/espressif/
-git clone --recursive https://github.com/espressif/esp-idf.git
+git clone -b ${VERSION}  --recursive https://github.com/espressif/esp-idf.git
 cd esp-idf/
-git checkout v4.0-beta2
 ./install.sh
 
