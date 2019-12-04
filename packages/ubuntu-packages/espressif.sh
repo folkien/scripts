6724fb5cf4fcb787f61b33a92e4adabee605fbfc
Sławomir Paszko <folkus@gmail.com>
2019-12-04 15:13:26 +0100

    Fixed espressif idf installation to 4.0 working version.

--- packages/ubuntu-packages/espressif.sh
+++ packages/ubuntu-packages/espressif.sh
@@ -1,4 +1,5 @@
-VERSION="v4.0-beta2"
+BRANCHVERSION="release/v4.0"
+VERSION="99fb9a3f7c28c5fa12b1bd4aa6fb7b622d841326"
 
 #Pre requistes
 sudo apt-get install -yf git wget flex bison gperf python python-pip python-setuptools python-serial python-click python-cryptography python-future python-pyparsing python-pyelftools cmake ninja-build ccache
@@ -6,7 +7,8 @@ mkdir -p /opt/espressif/
 
 # installation
 cd /opt/espressif/
-git clone -b ${VERSION}  --recursive https://github.com/espressif/esp-idf.git
+git clone -b ${BRANCHVERSION} --recursive https://github.com/espressif/esp-idf.git
 cd esp-idf/
+git checkout ${VERSION}
 ./install.sh
 
