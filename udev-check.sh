c843b81e682c73aa3e259320419343c55062a984
Sławomir Paszko <folkus@gmail.com>
2019-12-05 11:47:14 +0100

    Udev check fixes.

--- udev-check.sh
+++ udev-check.sh
@@ -7,7 +7,8 @@ fi
 device="${1}"
 
 # Get Device Path
-devPath=$(udevadm info --name /dev/ttyACM0 --query all | grep DEVPATH | cut -c 13-)
+devPath=$(udevadm info --name ${device} --query all | grep "DEVPATH\=.*" -o | cut -c 9-)
+echo "DEVPATH=${devPath}"
 
 # Query device rules
 udevadm test ${devPath}
