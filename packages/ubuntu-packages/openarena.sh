9a48de03008d9522170e98704063bf72a100509b
Sławomir Paszko <folkus@gmail.com>
2019-12-10 16:49:41 +0100

    Added ubuntu bug fixing.

--- packages/ubuntu-packages/openarena.sh
+++ packages/ubuntu-packages/openarena.sh
@@ -8,5 +8,13 @@ done
 DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 
 sudo apt install -yf openarena
+#Link settings
 rm -rf ~/.openarena/
 ln -sfv ${DIR}/openarena ~/.openarena
+
+# Fix ubuntu bug
+cd /tmp
+wget http://cz.archive.ubuntu.com/ubuntu/pool/universe/i/ioquake3/ioquake3_1.36+u20160122+dfsg1-1_amd64.deb -O ioquake.deb
+ar x ioquake.deb data.tar.xz
+tar Jxf data.tar.xz
+sudo mv ./usr/lib/ioquake3/ioquake3 /usr/lib/ioquake3/ioquake3
