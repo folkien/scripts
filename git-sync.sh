9a7d5e6fb534a5facedada26dc20eae721b74cec
Sławomir Paszko <folkus@gmail.com>
2019-12-10 12:21:32 +0100

    Git sync work.

--- git-sync.sh
+++ git-sync.sh
@@ -1,10 +1,20 @@
+source /etc/messages.sh
+
+# Check status
 git status
 [ $? -ne 0 ] && exit 0
-source /etc/messages.sh
-stashBefore=$(git stash | wc -l)
-git stash 
-stashAfter=$(git stash | wc -l)
+
+# Get branch name
+branch=$(git rev-parse --abbrev-ref HEAD)
+
+git stash
+
+# Number of diffrent commits
+diffrence=$(git log --oneline origin/${branch}..${branch} | wc -l)
+
+# Synchronize data
 git pull --rebase
 git push 
-[ ! ${stashBefore} -eq ${stashAfter} ] && git stash apply
+
+git stash apply
 minfo 'Updated log :' && git log --pretty=oneline | head -n 5
