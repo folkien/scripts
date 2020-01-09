35a44d5ad990565de5eacf62b954ce622819b644
Sławomir Paszko <folkus@gmail.com>
2020-01-09 15:16:28 +0100

    Added faster python script to make lines.

---
+++ csv-add-percent-column.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python2.7
+import sys
+
+def file_len(fname):
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
+
+if len(sys.argv)<2:
+    print "Give file name"
+    sys.exit(-1)
+
+
+filepath=sys.argv[1]
+outputFile="percent"+filepath
+lineNumber=1
+lineCount=file_len(filepath)
+
+fwrite=open(outputFile,'w+')
+with open(filepath,'r+') as fp:
+   line = fp.readline()
+   while line:
+    fwrite.write("%2.2f %s" % (float(lineNumber*100)/lineCount,line))
+    lineNumber+=1
+    line = fp.readline()
+
+fwrite.close()
