aa214ceed1ea3288b7b6317a158c0806d82249ce
Sławomir Paszko <folkus@gmail.com>
2020-01-24 09:44:11 +0100

    Added path argument.

--- git-branch-diff-from-master.sh
+++ git-branch-diff-from-master.sh
@@ -1,1 +1,1 @@
-git log --graph --pretty=format:'%Cred%h%Creset %aN -%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset' --abbrev-commit --date=relative $1..master
+git log --graph --pretty=format:'%Cred%h%Creset %aN -%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset' --abbrev-commit --date=relative $1..master $2
--- git-branch-diff-to-master.sh
+++ git-branch-diff-to-master.sh
@@ -1,1 +1,1 @@
-git log --graph --pretty=format:'%Cred%h%Creset %aN -%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset' --abbrev-commit --date=relative master..$1
+git log --graph --pretty=format:'%Cred%h%Creset %aN -%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset' --abbrev-commit --date=relative master..$1 $2
