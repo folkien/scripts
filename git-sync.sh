4fa4d6bf915edea5f9b45fdd825945acf4b70e01
Sławomir Paszko <folkus@gmail.com>
2020-01-30 13:43:34 +0100

    Git sync update.

--- git-sync.sh
+++ git-sync.sh
@@ -1,20 +1,20 @@
 source /etc/messages.sh
 
-# Check status
-git status
-[ $? -ne 0 ] && exit 0
+# Exit if not git repository
+[ $(git-is-repository) -eq 0 ] && echo "Not git repository!" && exit -1
 
 # Get branch name
 branch=$(git rev-parse --abbrev-ref HEAD)
-
-git stash
-
+# Get if there are local changes
+localChanges=$(git-are-changes)
 # Number of diffrent commits
 diffrence=$(git log --oneline origin/${branch}..${branch} | wc -l)
 
+
+[ ${localChanges} -eq 1 ] && git stash
 # Synchronize data
 git pull --rebase
 git push 
+[ ${localChanges} -eq 1 ] && echo "Applying stash!" && git stash apply > /dev/null
 
-git stash apply
 minfo 'Updated log :' && git log --pretty=oneline | head -n 5
