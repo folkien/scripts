cc38ff39763769b9bb7b617a91d8d54a83cd20e9
Sławomir Paszko <folkus@gmail.com>
2020-01-30 13:59:37 +0100

    Git sync update.

--- git-sync.sh
+++ git-sync.sh
@@ -9,6 +9,14 @@ branch=$(git rev-parse --abbrev-ref HEAD)
 localChanges=$(git-are-changes)
 # Number of diffrent commits
 diffrence=$(git log --oneline origin/${branch}..${branch} | wc -l)
+# local top commit
+localTop=$(git rev-parse HEAD)
+# last pushed top commit
+pushedTop=$(git rev-parse origin/${branch})
+git fetch origin ${branch}
+# last origin top commit
+originTop=$(git rev-parse origin/${branch})
+
 
 
 [ ${localChanges} -eq 1 ] && git stash
@@ -18,8 +26,7 @@ git push
 [ ${localChanges} -eq 1 ] && echo "Applying stash!" && git stash apply > /dev/null
 
 minfo "Pushed changes:"
-#TODO
+git log --pretty=oneline ${originTop}..${localTop}
 minfo "Pulled changes"
-# TODO
-
+git log --pretty=oneline ${pushedTop}..${originTop}
 minfo 'Updated log :' && git log --pretty=oneline | head -n 5
