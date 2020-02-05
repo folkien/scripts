fcd86bf6a20b4441e649d9f581266aa8621f5c0d
Sławomir Paszko <folkus@gmail.com>
2020-02-05 12:14:15 +0100

    Update of git-cm-update.sh, .

--- git-cm-update.sh
+++ git-cm-update.sh
@@ -2,11 +2,12 @@
 git add -p
 # Get filnames of adding files
 files=""
-git status -s | grep "^A. .*" | while read -r filePath;
+while read filePath;
 do
     file=$(echo ${filePath} | grep -o " .*")
     filename=$(basename ${file})
-    files="${files}, ${filename}"
-done
+    files="${files}${filename}, "
+done <<< $(git status -s | git status -s | grep -E "(A|AM|M|MM) .*")
+
 git commit -m "Update of ${files}."
 git-edit 3
