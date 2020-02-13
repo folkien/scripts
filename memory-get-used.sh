96d8d8d36dd6fa3719736149f266436d33712c65
Sławomir Paszko <folkus@gmail.com>
2020-02-13 11:49:01 +0100

    Memory get used fix.

--- memory-get-used.sh
+++ memory-get-used.sh
@@ -1,6 +1,8 @@
 file=${@}
 if [ -e ${file} ]; then
-    grep -o -oa -Pe "UsedMem: [0-9]* " ${file} | cut -c 9-  > ${file}.memory.csv
+    grep -o -a -e "\[.*\] UsedMem: [0-9]*" ${file} > ${file}.memory.csv
+    sed -i "s/\] UsedMem://g" ${file}.memory.csv
+    sed -i "s/\[//g" ${file}.memory.csv
 else
     echo "File doesn't exists!"
 fi
