9bf72834d6843ab75194ef34f58c5211c2a07897
Sławomir Paszko <folkus@gmail.com>
2020-02-13 12:37:04 +0100

    Used mem fix.

--- memory-get-used.sh
+++ memory-get-used.sh
@@ -1,6 +1,6 @@
 file=${@}
 if [ -e ${file} ]; then
-    grep -o -a -e "\[.*\] UsedMem: [0-9]*" ${file} > ${file}.memory.csv
+    grep -o -a -e "\[[0-9]*\] UsedMem: [0-9]*" ${file} > ${file}.memory.csv
     sed -i "s/\] UsedMem://g" ${file}.memory.csv
     sed -i "s/\[//g" ${file}.memory.csv
 else
