a01581b5781385a4e0048b8396cd9f5a63c965dc
Sławomir Paszko <folkus@gmail.com>
2020-02-24 11:32:14 +0100

    Fix of git-merge-diff.sh, .

--- git-merge-diff.sh
+++ git-merge-diff.sh
@@ -1,5 +1,5 @@
 # Merge diff like gitlab diff of incoming changes.
 currentBranch=$(git-branch-current)
-mergeBranch=$(${1})
+mergeBranch="${1}"
 
 git merge-tree $(git merge-base ${currentBranch} ${mergeBranch}) ${currentBranch} ${mergeBranch} | colordiff | less -R
