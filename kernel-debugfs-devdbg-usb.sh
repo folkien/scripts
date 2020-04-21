1afb495c3e8ca59313a4872b8ce461e1e67a73dc
Sławomir Paszko <folkus@gmail.com>
2020-04-21 15:46:05 +0200

    Fixes.

--- kernel-debugfs-devdbg-usb.sh
+++ kernel-debugfs-devdbg-usb.sh
@@ -4,12 +4,13 @@ echo "file hid-core.h +p" > /sys/kernel/debug/dynamic_debug/control
 echo "file hid-pidff.h +p" > /sys/kernel/debug/dynamic_debug/control
 # CORE
 echo "file hcd-pci.c +p" > /sys/kernel/debug/dynamic_debug/control
-echo "file urb.c +p" > /sys/kernel/debug/dynamic_debug/control
+echo "file usb.c +pfl" > /sys/kernel/debug/dynamic_debug/control
+echo "file urb.c +pfl" > /sys/kernel/debug/dynamic_debug/control
 echo "file usb-acpi.c +p" > /sys/kernel/debug/dynamic_debug/control
 # NET
 echo "file rndis_host.c +p" > /sys/kernel/debug/dynamic_debug/control
 echo "file cdc_ether.c +p" > /sys/kernel/debug/dynamic_debug/control
-echo "file cdc-acm.c +p" > /sys/kernel/debug/dynamic_debug/control
+echo "file cdc-acm.c +pfl" > /sys/kernel/debug/dynamic_debug/control
 # XHCI controller
 echo "file xhci-dbg.c +p" > /sys/kernel/debug/dynamic_debug/control
 echo "file xhci.c +p" > /sys/kernel/debug/dynamic_debug/control
