1e51f82605c4a85b081ea2dda332deec64755386
Sławomir Paszko <folkus@gmail.com>
2020-08-13 00:15:24 +0200

    Added nvidia package.

---
+++ packages/ubuntu-packages/nvidia.sh
@@ -0,0 +1,6 @@
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt update
+sudo apt-get install ubuntu-drivers-common
+ubuntu-drivers devices
+sudo ubuntu-drivers autoinstall
+sudo apt install nvtop
