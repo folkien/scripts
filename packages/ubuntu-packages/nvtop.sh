6277516e5a23b7c277a5f9cffd6759114009aaac
Sławomir Paszko <folkus@gmail.com>
2020-08-14 10:07:02 +0200

    Added nvtop install.

---
+++ packages/ubuntu-packages/nvtop.sh
@@ -0,0 +1,10 @@
+git clone https://github.com/Syllo/nvtop.git
+mkdir -p nvtop/build && cd nvtop/build
+cmake ..
+
+# If it errors with "Could NOT find NVML (missing: NVML_INCLUDE_DIRS)"
+# try the following command instead, otherwise skip to the build with make.
+cmake .. -DNVML_RETRIEVE_HEADER_ONLINE=True
+
+make
+sudo make install # You may need sufficient permission for that (root)
