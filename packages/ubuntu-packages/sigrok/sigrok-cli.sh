c03e74e8a35f4ab19fb14f033e399123668874a3
Sławomir Paszko <folkus@gmail.com>
2020-10-07 12:03:16 +0200

    fix.

--- packages/ubuntu-packages/sigrok/sigrok-cli.sh
+++ packages/ubuntu-packages/sigrok/sigrok-cli.sh
@@ -1,6 +1,6 @@
 sudo apt-get install -yf git-core gcc make autoconf automake libtool pkg-config libglib2.0-dev
-mkdir -p /opt/sigrok/
 git clone git://sigrok.org/sigrok-cli
+mkdir -p /opt/sigrok/
 if [ $? -eq 0 ]; then
      cd sigrok-cli
      git checkout sigrok-cli-0.7.0
