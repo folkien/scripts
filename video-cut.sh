ce9ab954f67e843adb70b54f31a4a2a516c27f7c
Sławomir Paszko <folkus@gmail.com>
2021-02-12 09:34:02 +0100

    Fix.

--- video-cut.sh
+++ video-cut.sh
@@ -1,11 +1,20 @@
 #/bin/bash
 file="${@}"
 from="00:00:00"
-to="00:01:00"
+from=$(\
+  dialog --title "Cut video from time :" \
+         --inputbox "Write time in format 00:00:00" 8 40 \
+         "00:00:00" \
+  3>&1 1>&2 2>&3 3>&- \
+)
+to="00:00:30"
 to=$(\
   dialog --title "Cut video to time :" \
          --inputbox "Write time in format 00:00:00" 8 40 \
+         "00:00:30" \
   3>&1 1>&2 2>&3 3>&- \
 )
-ffmpeg -ss ${from} -i ${file} -t ${to} -vcodec copy -acodec copy Cuted${file}
+
+echo "Cut from ${from} to ${to}."
+ffmpeg -ss ${from} -i "${file}" -t ${to} -vcodec copy -acodec copy Cuted${file}
 echo "Created Cuted${file}."
