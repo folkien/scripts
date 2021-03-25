8ae1370aff6a9da417edb73f8e24dd2b081aff6d
Sławomir Paszko <folkus@gmail.com>
2021-03-25 17:39:17 +0100

    Fixes.

--- nvidia-validate.sh
+++ nvidia-validate.sh
@@ -23,3 +23,34 @@ if [ $? -eq 0 ]; then
 else
     minfo "[Kernel] Valid, nouveau not inserted."
 fi
+
+# Check modprobe lists
+grep -r  /lib/modprobe.d/ -e "^blacklist nvidia"
+if [ $? -eq 0 ]; then
+    merror "[Kernel] Blacklisted nvidia!"
+else
+    minfo "[Kernel] Valid modprobe list."
+fi
+
+grep -r  /lib/modprobe.d/ -e "^alias nvidia off"
+if [ $? -eq 0 ]; then
+    merror "[Kernel] nvidia is off!"
+else
+    minfo "[Kernel] Valid modprobe list."
+fi
+
+# Check if module is inserted
+lsmod | grep "nvidia"
+if [ $? -ne 0 ]; then
+    merror "[Kernel] Module not inserted!"
+else
+    minfo "[Kernel] Module inserted."
+fi
+
+# CUDA check
+nvcc --version
+if [ $? -ne 0 ]; then
+    merror "[CUDA] CUDA compiler not exists!"
+else
+    minfo "[CUDA] Valid compiler."
+fi
