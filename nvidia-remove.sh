5f3b87b5fd161429616c35952a14f4deddf7a100
Sławomir Paszko <folkus@gmail.com>
2021-03-25 18:18:41 +0100

    Fixes.

--- nvidia-remove.sh
+++ nvidia-remove.sh
@@ -1,3 +1,9 @@
 sudo apt remove --purge cuda-*
 sudo apt remove --purge nvidia-*
 #sudo apt remove --purge
+
+sudo apt-get -o Dpkg::Options::="--force-overwrite" --fix-broken install
+
+
+# Install again
+#sudo apt-get -o Dpkg::Options::="--force-overwrite" install cuda-10-0
