664c71abff465168db8b7970fc3ee5772b41f48f
Sławomir Paszko <folkus@gmail.com>
2021-11-13 17:37:54 +0100

    Fixes.

--- ip-ssh-find.sh
+++ ip-ssh-find.sh
@@ -2,4 +2,4 @@
 gatewayIP=$(route -n | grep UG | xargs -d " " -I {} echo {} | grep 0.0.0.0 -v | grep -E "([0-9]{1,3}[\.]){3}[0-9]{1,3}")
 ipArray=(${gatewayIP//./ })
 echo "Gateway IP - ${ipArray[0]}.${ipArray[1]}.${ipArray[2]}.${ipArray[3]}."
-sudo nmap -sS -p 22 ${ipArray[0]}.${ipArray[1]}.${ipArray[2]}.1-255 | grep -e report -e ssh
+sudo nmap -sS -p 22 ${ipArray[0]}.${ipArray[1]}.${ipArray[2]}.1-255 |  grep open -B 4
