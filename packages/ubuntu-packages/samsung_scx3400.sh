a7b947b221c026de97434aa4834e9bc87025caae
Sławomir Paszko <folkus@gmail.com>
2022-03-10 17:02:38 +0100

    Fixes.

--- packages/ubuntu-packages/samsung_scx3400.sh
+++ packages/ubuntu-packages/samsung_scx3400.sh
@@ -1,6 +1,6 @@
 #install if not exists or given arg
 if [ ! -e /opt/samsung ] || [ "$1" == "all" ]; then
-    axel http://downloadcenter.samsung.com/content/DR/201407/20140709152519621/ULD_V1.00.27.04.tar.gz
+    wget http://downloadcenter.samsung.com/content/DR/201407/20140709152519621/ULD_V1.00.27.04.tar.gz
     tar -xvf ULD_V1.00.27.04.tar.gz
     cd uld
     sudo sh install.sh
