ccb2540669d8950cd67ca02f987e5592843180e3
Sławomir Paszko <folkus@gmail.com>
2023-02-16 16:59:25 +0100

    Fixes for video reduce code.

--- video-reduceh265.sh
+++ video-reduceh265.sh
@@ -1,5 +1,19 @@
 #/bin/bash
-file="${@}"
+# Check : missing args
+if [ $# -eq 0 ]; then
+    echo 'Missing arguments [file] [compresion = 42]'
+    exit -1
+fi
+
+# Read file
+if [ $# -gt 0 ]; then
+    file="$1"
+fi
+
+compression="42"
+if [ $# -gt 1 ]; then
+    compression="$2"
+fi
 
 echo "Reduction to h265 of file ${file}."
-ffmpeg -i "${file}" -c:v libx265 -an -crf 42 ${file}.h265.mp4
+ffmpeg -i "${file}" -c:v libx265 -an -crf ${compression} ${file}.h265.mp4
