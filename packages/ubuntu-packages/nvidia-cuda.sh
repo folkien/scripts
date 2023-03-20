757d67a273847adc03e1196df661937cf7f8146f
Sławomir Paszko <folkus@gmail.com>
2023-03-20 17:09:43 +0100

    Fixes.

--- packages/ubuntu-packages/nvidia-cuda.sh
+++ packages/ubuntu-packages/nvidia-cuda.sh
@@ -1,16 +1,22 @@
-# Add the package repositories
-sudo add-apt-repository ppa:graphics-drivers
 
 source /etc/os-release
 versionCode=$(echo ${VERSION_ID} | sed 's/\.//g')
 url=${ID}${versionCode}
+distro=${url}
+arch=$(uname -m)
 
+echo "Repository URL code is '${url}'."
+sleep 1
+
+# Add the package repositories
+sudo add-apt-repository ppa:graphics-drivers
 sudo apt-key adv --fetch-keys  http://developer.download.nvidia.com/compute/cuda/repos/${url}/x86_64/7fa2af80.pub
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distro/$arch/3bf863cc.pub
 wget https://developer.download.nvidia.com/compute/cuda/repos/${url}/x86_64/cuda-${url}.pin
 sudo mv cuda-${url}.pin /etc/apt/preferences.d/cuda-repository-pin-600
 
-sudo bash -c 'echo "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64 /" > /etc/apt/sources.list.d/cuda.list'
-sudo bash -c 'echo "deb http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64 /" > /etc/apt/sources.list.d/cuda_learn.list'
+sudo apt-add-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/${url}/x86_64 /" 
+sudo apt-add-repository "deb http://developer.download.nvidia.com/compute/machine-learning/repos/${url}/x86_64 /"
 
 sudo apt-get update
 sudo apt-get install cuda
