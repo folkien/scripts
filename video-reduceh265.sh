d009d53968cd0a83dfe9eec5bc8d9ffae0e97ed2
Sławomir Paszko <folkus@gmail.com>
2024-10-30 13:37:31 +0100

    Fixes.

--- video-reduceh265.sh
+++ video-reduceh265.sh
@@ -1,19 +1,20 @@
-#/bin/bash
+#!/bin/bash
+
 # Check : missing args
 if [ $# -eq 0 ]; then
-    echo 'Missing arguments [file] [compresion = 42]'
-    exit -1
+    echo 'Missing arguments [file] [compression = 42]'
+    exit 1
 fi
 
-# Read file
+# First argument is the file
+file="$1"
+shift
+
+# Rest of the arguments are the codec
+codec="libx265 -crf 42"
 if [ $# -gt 0 ]; then
-    file="$1"
+    codec="$@"
 fi
 
-compression="42"
-if [ $# -gt 1 ]; then
-    compression="$2"
-fi
-
-echo "Reduction to h265 of file ${file}."
-ffmpeg -i "${file}" -c:v libx265 -an -crf ${compression} ${file}.h265.mp4
+echo "Reduction to h265 of file ${file} with arguments : ${codec}."
+ffmpeg -i "${file}" -an -c:v ${codec} "${file}.h265.mp4"
