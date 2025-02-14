93d336d88be4813f8a70c60e4db6c8131e89e187
Sławomir Paszko <folkus@gmail.com>
2025-02-14 11:16:40 +0100

    Fixes.

--- image-resize.sh
+++ image-resize.sh
@@ -11,19 +11,19 @@
 #
 # Przykład użycia
 # ./resize.sh /images/ 800
-FILES=$1
+FILES="$1"
 
 # Szerokość nowego zdjęcia.
 if [ "$#" -gt 1 ] ; then
-		WIDTH=$2
+		WIDTH="$2"
 else
 		WIDTH=800
 fi
 
 function convertPhoto {
         # Resize file and convert to jpeg.
-		FILE=$1
-		WIDTH=$2
+		FILE="$1"
+		WIDTH="$2"
         filepath_noext="${FILE%.*}"
         filename=$(basename $FILE)
         filename_noext="${filename%.*}"
@@ -51,4 +51,4 @@ function convertPhoto {
 export -f convertPhoto
 
 #Petla główna
-find $FILES -mindepth 1 -maxdepth 2 -type f -print0 | xargs -0 -i bash -c 'convertPhoto "$@"' _ {} $WIDTH
+find "$FILES" -mindepth 1 -maxdepth 2 -type f -print0 | xargs -0 -i bash -c 'convertPhoto "$@"' _ {} $WIDTH
